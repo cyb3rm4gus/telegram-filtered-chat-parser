04bf2cc492ee760b1f35149e92971c9c14958466
cyb3rm4gus <cybersec78@gmail.com>
2022-10-14 12:40:51 +0300

    added my own script wth bjk & whrs and large file support

--- telegram-chat-parser.py
+++ telegram-chat-parser.py
@@ -46,34 +46,34 @@ mention_types = [
 null_name_counter = 0
 
 def parse_telegram_to_csv(jdata):
-    
+
     if jdata.get("name") is None:
-        global null_name_counter 
+        global null_name_counter
         null_name_counter += 1
         chat_name = f"UnnamedChat-{null_name_counter}"
     else:
         chat_name = re.sub(r'[\W_]+', u'', jdata.get("name"), flags=re.UNICODE)
     output_filepath = f"{chat_name}.csv"
 
-    
+
     with open(output_filepath, "w", encoding="utf-8") as output_file:
         writer = csv.DictWriter(output_file, columns, dialect="unix", quoting=csv.QUOTE_NONNUMERIC)
         writer.writeheader()
-        
+
         for message in jdata["messages"]:
             if message["type"] != "message":
                 continue
-            
+
             msg_id = message["id"]
             sender = message["from"]
             sender_id = message["from_id"]
             reply_to_msg_id = message["reply_to_message_id"] if "reply_to_message_id" in message else -1
             date = message["date"].replace("T", " ")
             dt = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
-            
+
             msg_content = message["text"]
             msg_type = "text"
-            
+
             if "media_type" in message:
                 msg_type = message["media_type"]
                 if message["media_type"] == "sticker":
@@ -86,7 +86,7 @@ def parse_telegram_to_csv(jdata):
             elif "file" in message:
                 msg_type = "file"
                 msg_content = message["file"]
-            
+
             if "photo" in message:
                 msg_type = "photo"
                 msg_content = message["photo"]
@@ -97,13 +97,13 @@ def parse_telegram_to_csv(jdata):
                 msg_type = "location"
                 loc = message["location_information"]
                 msg_content = str(loc["latitude"]) + "," + str(loc["longitude"])
-            
+
             has_mention = 0
             has_email = 0
             has_phone = 0
             has_hashtag = 0
             is_bot_command = 0
-            
+
             if type(msg_content) == list:
                 txt_content = ""
                 for part in msg_content:
@@ -122,12 +122,12 @@ def parse_telegram_to_csv(jdata):
                             has_hashtag = 1
                         elif part["type"] == "bot_command":
                             is_bot_command = 1
-                        
+
                         txt_content += part["text"]
                 msg_content = txt_content
-            
+
             msg_content = msg_content.replace("\n", " ")
-            
+
             row = {
                 "msg_id"          : msg_id,
                 "sender"          : sender,
@@ -142,7 +142,7 @@ def parse_telegram_to_csv(jdata):
                 "has_hashtag"     : has_hashtag,
                 "is_bot_command"  : is_bot_command,
             }
-            
+
             writer.writerow(row)
 
     print(chat_name, "OK!")
@@ -158,14 +158,13 @@ if __name__ == "__main__":
         sys.exit()
 
     backup_filepath = sys.argv[1]
-    
+
     with open(backup_filepath, "r", encoding="utf-8") as input_file:
         contents = input_file.read()
         jdata = json.loads(contents)
-        
+
         if "chats" not in jdata:
             parse_telegram_to_csv(jdata)
         else:
             for chat in jdata["chats"]["list"]:
                 parse_telegram_to_csv(chat)
-        
